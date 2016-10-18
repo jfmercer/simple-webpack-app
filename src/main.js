54c13b5ae2b3f736b9460afdc3f08d20feda5b64
John F. Mercer <john.f.mercer@gmail.com>
2016-10-18 17:10:20 -0400

    Step Five: Add ES6 Support
    `babel` & the `babel-loader` add support for ES6. Note that `main.js`
    now uses `import` rather than `require()`. Test files are excluded from
    webpack for the time being because they will be loaded later in a
    separate test configuration.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-require('./js/require-me.js');
+// Polyfill libraries
+// Load these first, per Google's recommendation
+import 'babel-polyfill';
+
+import './js/require-me.js';
 
 console.log('Hello, webpack!');
