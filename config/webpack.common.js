2cf4beca72d28a07b23afb7b05224aa08f91b5d4
John F. Mercer <john.f.mercer@gmail.com>
2016-10-18 14:03:21 -0400

    Step Four: Load HTML
    By default, webpack bundles javascript. To get it to load other
    filetypes, loaders and plugins must be used. In this commit, `html-loader`
    tells webpack how to load HTML files, and `html-webpack-plugin`
    injects scripts and links into `index.html` so that they do not have to
    be loaded manually. Note that there are no `<script>` tags in the HTML
    source file, but they are injected after the build.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,21 +1,40 @@
 var webpack = require('webpack');
 var validate = require('webpack-validator');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
   build: path.resolve('./build'),
+  index: path.resolve('./src/index.html'),
   main: path.resolve('src/main.js')
 };
 
 const COMMON = {
   entry: {
     main: PATHS.main
-  }, 
+  },
 
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve('./build')
-  }
+  },
+
+  // Configure loaders for various filetypes
+  module: {
+    loaders: [{
+      test: /\.html$/,
+      loader: 'html'
+    }],
+  },
+
+  plugins: [
+    // HtmlWebpackPlugin injects scripts & links into index.html
+    // so that we don't have to do so manually
+    new HtmlWebpackPlugin({
+      template: PATHS.index,
+      showErrors: true // In prod, set this to false. The default is true
+    })
+  ]
 }
 
 module.exports = validate(COMMON);
