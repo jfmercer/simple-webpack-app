28d4f8b03373d2b81ad6dbbe0d7e80f1bfaaaa07
John F. Mercer <john.f.mercer@gmail.com>
2016-10-19 18:04:05 -0400

    Step Eight, Part II: Split off button chunk
    Using webpack's `require.ensure`, we can split off our fat button from
    `main.js`. Now, `main.js` has "lost weight", but it's still unusally
    big. What is causing that problem? Additionally, a great deal of jQuery
    code is going into our button chunk (`1.1.js`), which blurs the line
    between application code and vendor code. Could we cleanly separate
    vendor and application code and, at the same time, reduce the size of
    our application? Hmm . . .

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import 'babel-polyfill';
 
 // Project Files
 import './really-impressive-styles.css';
-import button from './components/button';
 
-button('Winston').render('#first');
-button('Churchill').render('#second');
-
-console.log('Hello, webpack!');
+if (document.querySelectorAll('div#prime-minister').length) {
+  require.ensure([], function chunkButtonComponent() {
+    const button = require('./components/button').default;
+    button('Winston').render('#first');
+    button('Churchill').render('#second');
+  });
+}
